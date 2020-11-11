66fca9a75f361b8217aa2f6f277d3fb918b8b6c4
Fan Yang <fanyang1148@gmail.com>
2020-11-11 17:37:37 -0500

    temp

--- ezcampus/src/components/Posts/Posts.jsx
+++ ezcampus/src/components/Posts/Posts.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import SinglePost from './SinglePost'
+
 export default class Posts extends Component {
     constructor(props) {
         super(props)
+        this.data = [
+            {
+                id: '2232f-usfe-2323f23-2fdsf',
+                creator: 'Jeff',
+                title: 'VOR 2b2b looking for housemate',
+                content: 'We are looking for someone to share our apartment with. Housemate 1 is a ucsd graduate student, Housemate2 is a ucsd undergrad. No alchol, drugs',
+                date: '11/10/2020',
+                views: 10,
+                likes: 8
+            }
+        ]
     }
 
 
@@ -10,7 +22,7 @@ export default class Posts extends Component {
     render() {
         return (
             <div>
-                liyuan jiayou
+                <SinglePost data={this.data[0]}></SinglePost>
             </div>
         )
     }
--- ezcampus/src/components/Posts/SinglePost.jsx
+++ ezcampus/src/components/Posts/SinglePost.jsx
@@ -3,12 +3,18 @@ import React, { Component } from 'react'
 export default class SinglePost extends Component {
     constructor(props) {
         super(props)
+        this.data = this.props.data
+    }
+    componentDidMount() {
+        console.log(this.data)
     }
 
     render() {
+        const {creator, title, content, views, likes, date, id} = this.data
+
         return (
             <div>
-                <h1>Wo shi {this.props.id}</h1>
+                <h1>{id}, {creator}, {title}, {content}</h1>
             </div>
         )
     }
